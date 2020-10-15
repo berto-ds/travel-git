6de346f7097e0738684b5d3b513969cbdd14e162
Berto <berto.ribeiro@outlook.com>
2020-10-15 20:07:10 +0100

    webpack lesson

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    entry: './app/assets/scripts/App.js',
+    output: {
+        filename: 'bundled.js',
+        path: __dirname + '/app'
+    },
+    mode: 'development',
+    
+}
